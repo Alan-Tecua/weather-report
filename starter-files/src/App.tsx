104b92504ee66a6deb27411d607c0fe3537f13cf
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-10-14 21:19:34 +0900

    added hook

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,67 +1,23 @@
-import { ChangeEvent, useEffect, useState } from 'react'
-import { optionType } from './types'
 import Search from './components/Search'
+import useForecast from './hooks/useForecast'
 
 const App = (): JSX.Element => {
-  const [term, setTerm] = useState<string>('')
-
-  const [city, setCity] = useState<optionType | null>(null)
-
-  const [options, setOptions] = useState<[]>([])
-
-  const getSearchOptions = (value: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
-        process.env.REACT_APP_API_KEY
-      }`
-    )
-      .then((response) => response.json())
-      .then((data) => setOptions(data)) //console.log({ data }) --> check lat and lon are visible
-  }
-
-  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value.trim()
-    setTerm(value)
-    if (value === '') return
-    getSearchOptions(value)
-  }
-
-  const getForecast = (city: optionType) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&untis=metric&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => console.log({ data }))
-    // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-  }
-  const onSubmit = () => {
-    if (!city) return
-    getForecast(city)
-  }
-
-  const onOptionSelect = (option: optionType) => {
-    setCity(option)
-    // console.log(option.name)
-  }
-
-  useEffect(() => {
-    if (city) {
-      setTerm(city.name)
-      setOptions([])
-    }
-  }, [city])
-
-  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast()
 
   return (
     <main className="flex justify-center items-center bg-zinc-900	h-[100vh] w-full">
-      <Search
-        term={term}
-        options={options}
-        onInputChange={onInputChange}
-        onOptionSelect={onOptionSelect}
-        onSubmit={onSubmit}
-      ></Search>
+      {forecast ? (
+        'LMAO'
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   )
 }
---
+++ starter-files/src/hooks/useForecast.ts
@@ -0,0 +1,64 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import { optionType } from '../types'
+
+const useForecast = () => {
+  const [term, setTerm] = useState<string>('')
+  const [city, setCity] = useState<optionType | null>(null)
+  const [options, setOptions] = useState<[]>([])
+  const [forecast, setForecast] = useState<null>(null)
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+        process.env.REACT_APP_API_KEY
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => setOptions(data)) //console.log({ data }) --> check lat and lon are visible
+  }
+
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.trim()
+    setTerm(value)
+    if (value === '') return
+    getSearchOptions(value)
+  }
+
+  const getForecast = (city: optionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&untis=metric&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => setForecast(data))
+    // https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  }
+  const onSubmit = () => {
+    if (!city) return
+    getForecast(city)
+  }
+
+  const onOptionSelect = (option: optionType) => {
+    setCity(option)
+    // console.log(option.name)
+  }
+
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name)
+      setOptions([])
+    }
+  }, [city])
+
+  return {
+    term,
+    options,
+    forecast,
+    onInputChange,
+    onOptionSelect,
+    onSubmit,
+  }
+
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+}
+
+export default useForecast
